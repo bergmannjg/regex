d9a06762ca1692d3f8dcf722c592926e1504e492
Jürgen Bergmann <bergmannj@posteo.de>
2024-04-01 10:42:40 +0200

    feat: use Lake.CLI

--- Main.lean
+++ Main.lean
@@ -1,6 +1,12 @@
+import Init.System.IO
+import Lake.Util.Cli
+import Lake.Util.MainM
+import Lake.CLI.Error
 import Regex
 import Regex.Backtrack
 
+open Lean
+open Lake
 open NFA
 open Syntax
 open Regex
@@ -12,10 +18,67 @@ instance : MonadLift (Except String) IO where
     | Except.ok res => pure res
     | Except.error e => throw $ .userError e
 
-private def compile (p : String): IO Unit := do
-  let re ← build p default ⟨true⟩
+structure RegexOptions where
+  verbosity : Lake.Verbosity := .quiet
+  wantsHelp : Bool := false
+  all : Bool := false
+  unescape : Bool := false
+  path : Option String := none
 
-  IO.println s!"{re.nfa}"
+abbrev CliMainM := ExceptT CliError MainM
+abbrev CliStateM := StateT RegexOptions CliMainM
+abbrev CliM := ArgsT CliStateM
+
+def CliM.run (self : CliM α) (args : List String) : BaseIO ExitCode := do
+  let main := self.run' args |>.run' {}
+  let main := main.run >>= fun | .ok a => pure a | .error e => MonadError.error e.toString
+  main.run
+
+def takeOptArg (opt arg : String) : CliM String := do
+  match (← takeArg?) with
+  | none => throw <| CliError.missingOptArg opt arg
+  | some v => pure v
+
+def regexShortOption : (opt : Char) → CliM PUnit
+| 'a' => modifyThe RegexOptions ({· with all := true})
+| 'f' => do let path ← takeOptArg "-f" "path"; modifyThe RegexOptions ({· with path})
+| 'h' => modifyThe RegexOptions ({· with wantsHelp := true})
+| 'u' => modifyThe RegexOptions ({· with unescape := true})
+| 'v' => modifyThe RegexOptions ({· with verbosity := Lake.Verbosity.verbose})
+| opt => throw <| CliError.unknownShortOption opt
+
+def regexLongOption : (opt : String) → CliM PUnit
+| "--help" => modifyThe RegexOptions ({· with wantsHelp := true})
+| "--verbose" => modifyThe RegexOptions ({· with verbosity := Lake.Verbosity.verbose})
+| opt =>  throw <| CliError.unknownLongOption opt
+
+def regexOption :=
+  option {
+    short := regexShortOption
+    long := regexLongOption
+    longShort := shortOptionWithArg regexShortOption
+  }
+
+def getWantsHelp : CliStateM Bool :=
+  (·.wantsHelp) <$> get
+
+def usage :=
+  "
+usage: inspect [OPTIONS] <COMMAND>
+
+COMMANDS:
+  ast <re>                    print the abstract syntax tree
+  hir <re>                    print the high level intermediate representation
+  captures <re> <s>           get first or all captures of <re> in <s>
+
+OPTIONS:
+  --help, -h                  print help
+  --verbose, -v               show verbose information
+  --all, -a                   get all captures
+  --file, -f <path>           path to file with haystack
+  --unescape, -u              unescape haystack"
+
+namespace regex
 
 private def unescapeString (s : String) : String :=
   ⟨loop s.data []⟩
@@ -34,29 +97,64 @@ where
     | '\\' :: 'r' :: tail => '\r' :: (loop tail acc)
     | head :: tail => head :: (loop tail acc)
 
-private def captures (re haystack : String) (verbose : Bool := false) (all : Bool := false)
-    (unescape : Bool := false) : IO Unit := do
-  let haystack := if unescape then unescapeString haystack else haystack
+def ast : CliM PUnit := do
+  match ← takeArg? with
+  | some re =>
+      let ast ← Ast.parse re
+      IO.println s!"Ast\n{ast}"
+  | none => throw <| CliError.missingArg "re"
 
-  if verbose then
-    IO.println s!"re '{re}' haystack chars '{haystack.data|>List.map (UInt32.intAsString ·.val)}'"
+def hir : CliM PUnit := do
+  match ← takeArg? with
+  | some re =>
+      let ast ← Ast.parse re
+      let hir ← Syntax.translate default ast
+      IO.println s!"Hir\n{hir}"
+  | none => throw <| CliError.missingArg "re"
+
+def compile : CliM PUnit := do
+  match ← takeArg? with
+  | some pat =>
+    let re ← build pat default ⟨true⟩
+    IO.println s!"{re.nfa}"
+  | none => throw <| CliError.missingArg "re"
+
+def captures : CliM PUnit := do
+  processOptions regexOption
+  let opts ← getThe RegexOptions
+  let (re, haystack) :=
+    ← match ← takeArg?, ← takeArg? with
+    | some re, some haystack => pure (re, haystack)
+    | none, _ => throw <| CliError.missingArg "regex"
+    | some re, none =>
+      match opts.path with
+      | some path => do
+        let haystack ← IO.FS.readFile path
+        pure (re, haystack)
+      | none => throw <| CliError.missingArg "haystack"
+
+  let haystack := if opts.unescape then unescapeString haystack else haystack
+  let isVerbose := opts.verbosity == Lake.Verbosity.verbose
+  if isVerbose then
+    let chars := haystack.data |> List.map (fun (c : Char) => (UInt32.intAsString c.val))
+    IO.println s!"re '{re}' haystack chars '{chars}'"
 
   let regex ← Regex.build re
-  if verbose then IO.println s!"nfa {regex.nfa}"
+  if isVerbose then IO.println s!"nfa {regex.nfa}"
 
   let nl := "\n"
 
-  if all
+  if opts.all
   then
-    let (msgs, arr) := Regex.Log.all_captures haystack.toSubstring regex verbose
-    IO.println s!"msgs {msgs |> Array.map (· ++ nl)}"
+    let (msgs, arr) := Regex.Log.all_captures haystack.toSubstring regex (isVerbose)
+    if msgs.size > 0 then IO.println s!"msgs {msgs |> Array.map (· ++ nl)}"
     match arr.size with
     | 0 => IO.println s!"captures: none"
     | _ =>
       IO.println s!"captures found: {arr.size}"
       arr |> Array.forM (fun captures => IO.println s!"{captures}")
   else
-    match Regex.Log.captures haystack.toSubstring regex ⟨0⟩ verbose with
+    match Regex.Log.captures haystack.toSubstring regex ⟨0⟩ (isVerbose) with
     | (msgs, none) =>
       if msgs.size > 0 then IO.println s!"msgs {msgs |> Array.map (· ++ nl)}"
       IO.println s!"captures: none"
@@ -64,35 +162,30 @@ private def captures (re haystack : String) (verbose : Bool := false) (all : Boo
       if msgs.size > 0 then IO.println s!"msgs {msgs |> Array.map (· ++ nl)}"
       IO.println s!"{captures}"
 
-def main (args : List String): IO Unit := do
-  try {
-    match args with
-    | ["ast", re] =>
-        let ast ← Ast.parse re
-        IO.println s!"Ast\n{ast}"
-    | ["hir", re] =>
-        let ast ← Ast.parse re
-        let hir ← Syntax.translate default ast
-        IO.println s!"Hir\n{hir}"
-    | ["compile", re] =>
-        compile re
-    | ["captures", re, haystack] =>
-        captures re haystack
-    | ["captures", "-uv", re, haystack] =>
-        captures re haystack (unescape := true) (verbose := true)
-    | ["captures", "-a", re, haystack] =>
-        captures re haystack (all := true)
-    | ["captures", "-av", re, haystack] =>
-        captures re haystack (verbose := true) (all := true)
-    | ["captures", "-v", re, haystack] =>
-        captures re haystack (verbose := true)
-    | ["captures", re, "-f", path] =>
-        let haystack ← IO.FS.readFile path
-        captures re haystack
-    | ["captures", "-a", re, "-f", path] =>
-        let haystack ← IO.FS.readFile path
-        captures re haystack (all := true)
-    | _ => IO.println
-            "usage: [ast <re>|[hir <re>|[compile <re>]|[captures [-v] [-a] [-av] <re> <s>]"
-  }
-  catch e => IO.println s!"error: {e}"
+end regex
+
+def regexCli : (cmd : String) → CliM PUnit
+| "ast" => regex.ast
+| "hir" => regex.hir
+| "compile" => regex.compile
+| "captures" => regex.captures
+| cmd => throw <| CliError.unknownCommand cmd
+
+def regex : CliM PUnit := do
+  match (← getArgs) with
+  | [] => IO.println usage
+  | _ =>
+    processLeadingOptions regexOption
+    if let some cmd ← takeArg? then
+      regexCli cmd
+    else
+      if (← getWantsHelp) then
+        IO.println usage
+      else
+        throw <| CliError.missingCommand
+
+def cli (args : List String) : BaseIO ExitCode :=
+  (regex).run args
+
+def main (args : List String) : IO UInt32 := do
+  cli args
