f7c51f16f29a3901141fa9cd09cfd963901b617b
J. Bergmann <bergmannj@posteo.de>
2024-05-06 16:07:50 +0200

    chore: update to v4.7.0

--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,14 @@
 import Lake
 open Lake DSL
 
-require UnicodeBasic from git "https://github.com/fgdorais/lean4-unicode-basic.git" @ "main"
-
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.7.0-rc2"
-
 meta if get_config? env = some "dev" then
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4"
+    @ "a34d3c1f7b72654c08abe5741d94794db40dbb2e"
+
+require UnicodeBasic from git "https://github.com/fgdorais/lean4-unicode-basic.git"
+    @ "02b5c10791d189630fde0428ce930ec0974f0a9f"
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.7.0"
 
 package «Regex» {
 }
